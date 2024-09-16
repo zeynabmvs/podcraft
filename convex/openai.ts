275684248a03b7bc6cf46d5c7c703b469ef3884b
zeynab.mvs <zeynab.mvs@gmail.com>
2024-09-16 08:42:38 +0330

    bug fix

--- convex/openai.ts
+++ convex/openai.ts
@@ -1,20 +1,24 @@
 import { action } from "./_generated/server";
 import { v } from "convex/values";
+
 import OpenAI from "openai";
 import { SpeechCreateParams } from "openai/resources/audio/speech.mjs";
 
-const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+})
 
 export const generateAudioAction = action({
   args: { input: v.string(), voice: v.string() },
   handler: async (_, { voice, input }) => {
     const mp3 = await openai.audio.speech.create({
       model: "tts-1",
-      voice: voice as SpeechCreateParams["voice"],
-      input: input,
+      voice: voice as SpeechCreateParams['voice'],
+      input,
     });
-    const buffer = Buffer.from(await mp3.arrayBuffer());
 
+    const buffer = await mp3.arrayBuffer();
+    
     return buffer;
   },
 });
