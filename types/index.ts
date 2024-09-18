0ccb7f3df975f803344d6930bfb3cb6ede50ae36
zeynab.mvs <zeynab.mvs@gmail.com>
2024-09-18 09:30:39 +0330

    podcast detail page

--- types/index.ts
+++ types/index.ts
@@ -1,6 +1,8 @@
 /* eslint-disable no-unused-vars */
-import { Id } from "@/convex/_generated/dataModel";
+
 import { Dispatch, SetStateAction } from "react";
+
+import { Id } from "@/convex/_generated/dataModel";
 
 export interface EmptyStateProps {
   title: string;
@@ -9,15 +11,43 @@ export interface EmptyStateProps {
   buttonLink?: string;
 }
 
-export interface PodcastCardProps {
+export interface TopPodcastersProps {
+  _id: Id<"users">;
+  _creationTime: number;
+  email: string;
+  imageUrl: string;
+  clerkId: string;
+  name: string;
+  podcast: {
+    podcastTitle: string;
     podcastId: Id<"podcasts">;
-    imgUrl: string;
-    title: string;
-    description: string;
+  }[];
+  totalPodcasts: number;
 }
 
-export interface PodcastDetailsProps {
-  id: Id<"podcasts">;
+export interface PodcastProps {
+  _id: Id<"podcasts">;
+  _creationTime: number;
+  audioStorageId: Id<"_storage"> | null;
+  user: Id<"users">;
+  podcastTitle: string;
+  podcastDescription: string;
+  audioUrl: string | null;
+  imageUrl: string | null;
+  imageStorageId: Id<"_storage"> | null;
+  author: string;
+  authorId: string;
+  authorImageUrl: string;
+  voicePrompt: string;
+  imagePrompt: string | null;
+  voiceType: string;
+  audioDuration: number;
+  views: number;
+}
+
+export interface ProfilePodcastProps {
+  podcasts: PodcastProps[];
+  listeners: number;
 }
 
 export interface GeneratePodcastProps {
@@ -38,14 +68,62 @@ export interface GenerateThumbnailProps {
   setImagePrompt: Dispatch<SetStateAction<string>>;
 }
 
-export interface GenerateThumbnailProps {
-  setImage: Dispatch<SetStateAction<string>>;
-  setImageStorageId: Dispatch<SetStateAction<Id<"_storage"> | null>>;
-  image: string;
-  imagePrompt: string;
-  setImagePrompt: Dispatch<SetStateAction<string>>;
+export interface LatestPodcastCardProps {
+  imgUrl: string;
+  title: string;
+  duration: string;
+  index: number;
+  audioUrl: string;
+  author: string;
+  views: number;
+  podcastId: Id<"podcasts">;
 }
 
-export interface PodcastGridProps {
-  items: PodcastCardProps[]
+export interface PodcastDetailPlayerProps {
+  audioUrl: string;
+  podcastTitle: string;
+  author: string;
+  isOwner: boolean;
+  imageUrl: string;
+  podcastId: Id<"podcasts">;
+  imageStorageId: Id<"_storage">;
+  audioStorageId: Id<"_storage">;
+  authorImageUrl: string;
+  authorId: string;
 }
+
+export interface AudioProps {
+  title: string;
+  audioUrl: string;
+  author: string;
+  imageUrl: string;
+  podcastId: string;
+}
+
+export interface AudioContextType {
+  audio: AudioProps | undefined;
+  setAudio: React.Dispatch<React.SetStateAction<AudioProps | undefined>>;
+}
+
+export interface PodcastCardProps {
+  imgUrl: string;
+  title: string;
+  description: string;
+  podcastId: Id<"podcasts">;
+}
+
+export interface CarouselProps {
+  fansLikeDetail: TopPodcastersProps[];
+}
+
+export interface ProfileCardProps {
+  podcastData: ProfilePodcastProps;
+  imageUrl: string;
+  userFirstName: string;
+}
+
+export type UseDotButtonType = {
+  selectedIndex: number;
+  scrollSnaps: number[];
+  onDotButtonClick: (index: number) => void;
+};
