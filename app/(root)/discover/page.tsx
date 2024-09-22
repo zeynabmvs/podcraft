e4b9ad42d3e312bca661dc438ee9dcbeee3ccc2d
zeynab.mvs <zeynab.mvs@gmail.com>
2024-09-22 18:42:28 +0330

    implement search

--- app/(root)/discover/page.tsx
+++ app/(root)/discover/page.tsx
@@ -1,36 +1,44 @@
-import PodcastCard from "@/components/PodcastCard";
-import Searchbar from "@/components/Searchbar";
-import EmptyState from "@/components/EmptyState";
-// import { fetchExplorePodcasts } from "@/lib/data";
+"use client"
 
-export default function Discover({ search }: {search: boolean}) {
-  // const podcastsData = await fetchExplorePodcasts();
+import EmptyState from '@/components/EmptyState'
+import LoaderSpinner from '@/components/LoaderSpinner'
+import PodcastCard from '@/components/PodcastCard'
+import Searchbar from '@/components/Searchbar'
+import { api } from '@/convex/_generated/api'
+import { useQuery } from 'convex/react'
+import React from 'react'
+
+const Discover = ({ searchParams: { search} }: { searchParams : { search: string }}) => {
+  const podcastsData = useQuery(api.podcasts.getPodcastBySearch, { search: search || '' })
 
   return (
     <div className="flex flex-col gap-9">
       <Searchbar />
       <div className="flex flex-col gap-9">
         <h1 className="text-20 font-bold text-white-1">
-          {!search ? "Discover Trending Podcasts" : "Search results for "}
+          {!search ? 'Discover Trending Podcasts' : 'Search results for '}
           {search && <span className="text-white-2">{search}</span>}
         </h1>
-        {/* {podcastsData ? (
+        {podcastsData ? (
           <>
             {podcastsData.length > 0 ? (
               <div className="podcast_grid">
-                {podcastsData?.map((item) => (
-                  <PodcastCard item={item} key={item.id} />
-                ))}
-              </div>
-            ) : (
-              <EmptyState title="No results found" />
-            )}
+              {podcastsData?.map(({ _id, podcastTitle, podcastDescription, imageUrl }) => (
+                <PodcastCard 
+                  key={_id}
+                  imgUrl={imageUrl!}
+                  title={podcastTitle}
+                  description={podcastDescription}
+                  podcastId={_id}
+                />
+              ))}
+            </div>
+            ) : <EmptyState title="No results found" />}
           </>
-        ) : (
-          "loading"
-          //   <LoaderSpinner />
-        )} */}
+        ) : <LoaderSpinner />}
       </div>
     </div>
-  );
+  )
 }
+
+export default Discover
--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -1,26 +1,35 @@
-'use client'
+"use client";
 
-import React, { useEffect, useState } from 'react'
-import { Input } from '@/components/ui/input'
-import Image from 'next/image'
-import { usePathname, useRouter } from 'next/navigation'
+import React, { useEffect, useState } from "react";
+import { Input } from "@/components/ui/input";
+import Image from "next/image";
+import { usePathname, useRouter } from "next/navigation";
+import { useDebounce } from "@/lib/useDebounce";
 
 const Searchbar = () => {
-  const [search, setSearch] = useState('');
-  const router = useRouter(); 
+  const [search, setSearch] = useState("");
+  const router = useRouter();
   const pathname = usePathname();
 
+  const debouncedValue = useDebounce(search, 500);
+
+  useEffect(() => {
+    if (debouncedValue) {
+      router.push(`/discover?search=${debouncedValue}`);
+    } else if (!debouncedValue && pathname === "/discover")
+      router.push("/discover");
+  }, [router, pathname, debouncedValue]);
 
   return (
     <div className="relative mt-8 block">
-      <Input 
+      <Input
         className="input-class py-6 pl-12 focus-visible:ring-offset-primary-1"
-        placeholder='Search for podcasts'
+        placeholder="Search for podcasts"
         value={search}
         onChange={(e) => setSearch(e.target.value)}
-        onLoad={() => setSearch('')}
+        onLoad={() => setSearch("")}
       />
-      <Image 
+      <Image
         src="/icons/search.svg"
         alt="search"
         height={20}
@@ -28,7 +37,7 @@ const Searchbar = () => {
         className="absolute left-4 top-3.5"
       />
     </div>
-  )
-}
+  );
+};
 
-export default Searchbar
+export default Searchbar;
