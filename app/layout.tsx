e474f2410c74be5e80270cee7f94d5a5dbde41e9
zeynab.mvs <zeynab.mvs@gmail.com>
2024-10-16 11:03:31 +0330

    Add error boundry

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Manrope } from "next/font/google";
 import ConvexClerkProvider from "@/providers/ConvexClerkProvider";
 import "@/app/globals.css";
 import AudioProvider from "@/providers/AudioProvider";
+import ErrorBoundary from "@/components/ErrorBoundry";
 
 const manrope = Manrope({ subsets: ["latin"] });
 
@@ -23,7 +24,9 @@ export default function RootLayout({
     <ConvexClerkProvider>
       <html lang="en">
         <body className={`${manrope.className} text-white-1 bg-black-3`}>
-          <AudioProvider>{children}</AudioProvider>
+          <ErrorBoundary>
+            <AudioProvider>{children}</AudioProvider>
+          </ErrorBoundary>
         </body>
       </html>
     </ConvexClerkProvider>
