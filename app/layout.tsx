749023d6080260f44d3de1a01f01ce0af25cd0dd
zeynab.mvs <zeynab.mvs@gmail.com>
2024-10-27 06:57:10 +0330

    chnage forn to Inter

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,11 @@
 import type { Metadata } from "next";
-import { Manrope } from "next/font/google";
+import { Manrope, Inter } from "next/font/google";
 import ConvexClerkProvider from "@/providers/ConvexClerkProvider";
 import "@/app/globals.css";
 import AudioProvider from "@/providers/AudioProvider";
 
 const manrope = Manrope({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Podcraft",
@@ -22,7 +23,7 @@ export default function RootLayout({
   return (
     <ConvexClerkProvider>
       <html lang="en">
-        <body className={`${manrope.className} text-white-1 bg-black-3`}>
+        <body className={`${inter.className} text-white-1 bg-black-3`}>
           <AudioProvider>{children}</AudioProvider>
         </body>
       </html>
